5862169ac91031c12bb695cf1bc91ad45274e5a9
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-11-02 14:29:51 -0600

    add: model, features, chip, footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
+import * as Sentry from '@sentry/react'
+import Features from "./components/Features"
+import Chip from "./components/Chip"
+import Footer from "./components/Footer"
 
 const App = () => {
 
+  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <Chip />
+      <Footer />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
+// export default App;
